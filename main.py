31ac1256ca63737ae47079174860e0c6ffdc0499
gildemberg-santos <gildemberg.santos@gmail.com>
2023-11-07 16:25:10 -0300

    feat: Incluir correção de pylint

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+""" Importação de bliblitecas para transformação de dados e criação do modelo """
 import pandas as pd
 from src.predict import Predict
 
-data = pd.read_csv("./data/exported_data.csv")
-modelo = './data/modelo'
-x = ['sale_amount', 'sale_vehicle_id','vehicle_year','vehicle_price', 'lead_bank_score', 'lead_have_children', 'lead_years_employment', 'lead_income']
-y = 'sale_sold'
-modelo_pkl = Predict(modelo, data, x, y)
+DATA = pd.read_csv("./data/exported_data.csv")
+MODELO = './data/modelo'
+X = [
+    'sale_amount', 'sale_vehicle_id','vehicle_year','vehicle_price', 'lead_bank_score', 
+    'lead_have_children', 'lead_years_employment', 'lead_income'
+]
+Y = 'sale_sold'
+modelo_pkl = Predict(MODELO, DATA, X, Y)
 modelo_pkl.save()
 modelo_pkl.load()
 print(modelo_pkl.accuracy)
